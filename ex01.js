015d73bb25515bbf7faea8ed06b2181f0833cb08
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-02 21:43:58 -0300

    adiciona o enunciado do exercicio 01 em forma de comentario

--- ex01.js
+++ ex01.js
@@ -1,6 +1,12 @@
 const prompt = require("prompt-sync")();
 console.clear();
 
+/* 
+Faça um programa que peça 2 números 
+e crie um array contando todos os números os números sequenciais 
+do primeiro até o segundo, independente de ser maior ou menor.
+*/
+
 let num1 = +prompt("Digite o primeiro número: ");
 let num2 = +prompt("Digite o segundo número: ");
 
